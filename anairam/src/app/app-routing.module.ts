3d7008c70ba7c4b32b5743f1028f493b6d1a5815
Natan Santana <natan.santana.dev@gmail.com>
2023-06-05 08:51:38 -0300

    Adicionando navegação

--- anairam/src/app/app-routing.module.ts
+++ anairam/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContagemRegressivaComponent } from './contagem-regressiva/contagem-regressiva.component';
+import { TempoNamoroComponent } from './tempo-namoro/tempo-namoro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/tempo-namoro', pathMatch: 'full'},
+  {path: 'tempo-namoro', component: TempoNamoroComponent},
+  { path: 'contagem-regressiva', component: ContagemRegressivaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
