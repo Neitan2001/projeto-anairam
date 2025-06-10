04603dc8c69a581b8554a70e2f7c2b3787b22d87
Natan Santana <natan.santana.dev@gmail.com>
2025-06-09 21:54:51 -0300

    feat: ajuste automático do scroll

--- src/app/linha-temporal/linha-temporal.component.ts
+++ src/app/linha-temporal/linha-temporal.component.ts
@@ -10,8 +10,26 @@ export class LinhaTemporalComponent implements OnInit {
   eventos: Evento[] = [];
   selectedEvent: Evento | null = null;
   hoveredIndex: number | null = null;
+  lastSelectedIndex: number | null = null;
 
-  constructor(private eventService: EventService) {}
+  constructor(private eventService: EventService) { }
+
+  selectEvent(event: Evento, index: number) {
+    this.selectedEvent = event;
+    this.lastSelectedIndex = index;
+  }
+
+  onCloseDetail() {
+    this.selectedEvent = null;
+    setTimeout(() => {
+      if (this.lastSelectedIndex !== null) {
+        const el = document.getElementById('marker-' + this.lastSelectedIndex);
+        if (el) {
+          el.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
+        }
+      }
+    }, 100);
+  }
 
   ngOnInit(): void {
     this.eventService.getEvents()
